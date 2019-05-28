f407d5c6657c3ff0e2dca5f70866a40b9f454d79
sangam-pt2905 <sangam-pt2905@csez.zohocorpin.com>
2019-05-28 08:28:41 +0530

    first commit

---
+++ public/js/country.js
@@ -0,0 +1,81 @@
+function main() {
+    document.getElementById("loader").style.display = "flex";
+    document.getElementById("invalid-country").classList.add("d-none");
+    document.getElementById("content").classList.add("d-none");
+    var slownetwork = setTimeout(function () {
+        document.getElementById("slow-network").classList.remove("d-none");
+    }, 5000);
+    var code = location.hash.substr(1);
+    document.querySelector("title").innerHTML += code;
+    var myRequest = new Request("https://restcountries.eu/rest/v2/alpha/" + code);
+    fetch(
+        myRequest
+    ).then(function (res) {
+        return res.json();
+    }).then(function (res) {
+        clearInterval(slownetwork);
+        console.log(res);
+        start(res);
+    }).catch(function () {
+        start(null);
+    });
+}
+main();
+window.onhashchange = main;
+function start(data) {
+    document.getElementById("loader").style.display = "none";
+    if (data == null || data.status == 400) {
+        document.getElementById("invalid-country").classList.remove("d-none");
+        return;
+    }
+    document.getElementById("content").classList.remove("d-none");
+    try {
+        document.getElementById("country-name").innerHTML = data.name;
+    } catch (e) { }
+    try {
+        document.getElementById("flag").src = data.flag;
+    } catch (e) { }
+    var keys = ["nativeName", "name", "capital", "subregion",
+        "region", "population", "demonym", "area", "alpha2Code",
+        "alpha3Code", "callingCodes", "latlng", "topLevelDomain",
+        "numericCode", "timezones", "gini"
+    ];
+    keys.forEach(key => {
+        try {
+            document.getElementById(key).innerHTML = data[key] || '-';
+        } catch (e) { }
+    });
+    document.getElementById("borders").innerHTML = "";
+    document.getElementById("languages").innerHTML = "";
+    document.getElementById("altSpellings").innerHTML = "";
+    document.getElementById("translations").innerHTML = "";
+    data.borders.forEach(border => {
+        try {
+            document.getElementById("borders").innerHTML += `<tr><td><a href='#${border}'>${border}</a></td></tr>`;
+        } catch (e) { }
+    });
+    if (data.borders.length == 0) {
+        document.getElementById("borders").innerHTML += `<tr><td>No Borders Found</td></tr>`;
+    }
+    data.languages.forEach(lang => {
+        try {
+            document.getElementById("languages").innerHTML += `<tr><td>${lang.name}</td></tr>`;
+        } catch (e) { }
+    });
+    if (data.languages.length == 0) {
+        document.getElementById("languages").innerHTML += `<tr><td>No languages Found</td></tr>`;
+    }
+    data.altSpellings.forEach(alt => {
+        try {
+            document.getElementById("altSpellings").innerHTML += `<tr><td>${alt}</td></tr>`;
+        } catch (e) { }
+    });
+    if (data.altSpellings.length == 0) {
+        document.getElementById("altSpellings").innerHTML += `<tr><td>No Alternate Spellings Found</td></tr>`;
+    }
+    for (key in data.translations) {
+        try {
+            document.getElementById("translations").innerHTML += `<tr><td class="h6">${key}</td><td>${data.translations[key]}</td></tr>`;
+        } catch (e) { }
+    }
+}
---
+++ public/js/home.js
@@ -0,0 +1,169 @@
+var slownetwork = setTimeout(function () {
+    console.log("called");
+    document.getElementById("slow-network").classList.remove("d-none");
+}, 5000);
+var countries = [];
+var myRequest = new Request("https://restcountries.eu/rest/v2/all");
+fetch(
+    myRequest
+).then(function (res) {
+    return res.json();
+}).then(function (res) {
+    clearTimeout(slownetwork);
+    countries = res;
+    start();
+}).catch(function () {
+    // start(null);
+});
+var keywords = {};
+function start() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("content").classList.remove("d-none");
+    var cardDeck = document.getElementById("card-deck");
+    var html = `
+    <div class="card">
+        <div class="card-header d-flex">
+            <div>
+                <div class="font-weight-bold">{{name}}</div>
+                <small>{{subregion}}, {{region}}</small>
+            </div>
+            <img src="{{flag}}" class="flag ml-auto">
+        </div>
+        <div class="card-body">
+            <table class="table-bordered" width="100%">
+                <tr>
+                    <td class="font-italic">Capital</td>
+                    <td>{{capital}}</td>
+                </tr>
+                <tr>
+                    <td class="font-italic">Native Name</td>
+                    <td>{{nativeName}}</td>
+                </tr>
+                <tr>
+                    <td class="font-italic">Population</td>
+                    <td>{{population}}</td>
+                </tr>
+                <tr>
+                    <td class="font-italic">Code</td>
+                    <td>{{alpha3Code}}</td>
+                </tr>
+            </table>
+        </div>
+        <!-- <div class="card-footer"></div> -->
+    </div>`;
+    // html = html.replace("d-none", "");
+    var tags = ["name", "flag", "subregion", "region",
+        "capital", "nativeName", "population", "alpha3Code"];
+    var btags = [0, 2, 3, 4, 7];
+    for (var i = 0; i < countries.length; i++) {
+        var code = html;
+        tags.forEach(tag => {
+            code = code.replace(`{{${tag}}}`, countries[i][tag]);
+        });
+        var div = document.createElement("div");
+        div.onclick = function () {
+            redirect(this.id);
+        }
+        div.classList.add('country');
+        div.classList.add('col-md-6');
+        div.id = countries[i]['alpha3Code'];
+        div.innerHTML = code;
+        cardDeck.appendChild(div);
+        // cardDeck.innerHTML += code;
+        btags.forEach(ind => {
+            buildTrie(countries[i][tags[ind]].toLowerCase(), keywords, i);
+            //inserting specific values like country name,region,etc. to Trie node
+        });
+    }
+    console.log(keywords);
+}
+function search(str, keywords, res, i = 0) {
+    /*
+    Search the string in Trie data structure node for fast retrival
+    */
+    if (i == str.length) {
+        return 200;
+    }
+    var c = str[i];
+    if (!(c in keywords)) {
+        return 404;
+    }
+    res.inds = keywords[c][1];
+    return search(str, keywords[c][0], res, i + 1);
+}
+function filter(str) {
+    /*
+    Get results from Trie node for corresponding user query and show results if any,or show the suggestions if any
+    */
+    str = str.trim().toLowerCase(); //string trimming
+    document.getElementById("alert-invalid-text").classList.add('d-none');
+    document.getElementById('suggestions-tab').classList.add('d-none');
+    if (str.length == 0) {
+        //On empty string,show all fetched results 
+        var elements = document.getElementsByClassName("country");
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].style.display = "inline";
+        }
+        return;
+    }
+    var res = { inds: [] };
+    var rcode = search(str, keywords, res);//start search in Trie node
+    var elements = document.getElementsByClassName("country");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = "none";
+    }
+    if (rcode == 404) {
+        document.getElementById("invalid-text").innerHTML = str;
+        document.getElementById("alert-invalid-text").classList.remove('d-none');
+        return showSuggestions(res.inds);
+    }
+    console.info("Results", res);
+    res.inds.forEach(ind => {
+        document.getElementById(countries[ind].alpha3Code).style.display = "inline";
+    });
+}
+
+function buildTrie(name, dic, ind, i = 0) {
+    /*
+    Insert the string to Trie data structure node for fast retrival
+    */
+    if (i == name.length) {
+        return;
+    }
+    var c = name[i];
+    if (!(c in dic)) {
+        dic[c] = [{}, new Set()];
+    }
+    dic[c][1].add(ind);
+    buildTrie(name, dic[c][0], ind, i + 1);
+}
+function redirect(code) {
+    /*
+    On click filtered results, redirect to country.html with coutry code
+    */
+    location.assign("country.html#" + code);
+}
+function showSuggestions(inds) {
+    /*
+    Show suggestions on invalid text entered
+    */
+    if (inds.length == 0) {
+        return;
+    }
+    document.getElementById("suggestions-tab").classList.remove('d-none');
+    document.getElementById("suggestions").innerHTML = "";
+    var html = [], i = 0;
+    inds.forEach(ind => {
+        if (i == 6) return;
+        html.push(` <a href=javascript:change('${countries[ind].name}')>${countries[ind].name}</a>`);
+        i++;
+    });
+    document.getElementById("suggestions").innerHTML = html;
+}
+function change(str) {
+    /*
+    Set clicked suggestion as input and start filter results
+    */
+    document.getElementById("search-bar").value = str;
+    filter(str);
+}
